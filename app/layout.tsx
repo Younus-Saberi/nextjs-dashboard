a0b07c2288a6b259dcf27bdc380c27f3d7085147
Younus-TradeBrains <younus.saberi@tradebrains.in>
2024-04-25 15:05:32 +0530

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,8 @@
+import '@/app/ui/global.css';
+import { Inter, } from 'next/font/google';
+import SideNav from '@/app/ui/dashboard/sidenav';
+ 
+export const inter = Inter({ subsets: ['latin'] });
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +10,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+
+      <body className={`${inter.className} antialiased`}>{children}
+      <SideNav/>
+      </body>
+
     </html>
   );
 }
